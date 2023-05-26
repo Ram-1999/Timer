29800618e64d18c53887a39bbc913cb06dc2cfb9
ram <kanuriramakrishna99@gmail.com>
2023-05-26 17:15:15 +0530

    changes made

--- countdown.js
+++ countdown.js
@@ -1,16 +1,13 @@
-var start = function Timer() {
-        var udateElement = document.getElementById("datedata");
-        var udate = udateElement.value;
+var start = function Timer(){
+
+        var udate = document.getElementById("datedata").value
         var countdown = new Date(udate).getTime();
-        var currentDate = new Date().getTime();
-      
-        if (countdown < currentDate) {
-          document.getElementById("over").innerHTML = "Please enter the present/future date";
-          udateElement.setAttribute("disabled", "disabled"); 
-          return; 
+        var date = new Date().getTime();
+        if(countdown<date){
+                document.getElementById("over").innerHTML = "Please enter the present/future date";
         }
-      
-        var distance = countdown - currentDate;
+        else{
+        var distance = countdown - date;
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -19,11 +16,10 @@ var start = function Timer() {
         document.getElementById("hourp").innerHTML = hours;
         document.getElementById("minp").innerHTML = minutes;
         document.getElementById("secp").innerHTML = seconds;
-      
-        udateElement.removeAttribute("disabled"); 
-      }
-      
-      function stopClock() {
+        }
+
+}
+
+function stopClock() {
         location.reload();
-      }
-      
+ }
