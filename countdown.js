6c4a20149cf2c0c36729cd141cc9ef4b897e2037
unknown <vamsijvk0337@gmail.com>
2023-05-26 12:55:30 +0530

    Updated code

--- countdown.js
+++ countdown.js
@@ -1,13 +1,16 @@
-var start = function Timer(){
-
-        var udate = document.getElementById("datedata").value
+var start = function Timer() {
+        var udateElement = document.getElementById("datedata");
+        var udate = udateElement.value;
         var countdown = new Date(udate).getTime();
-        var date = new Date().getTime();
-        if(countdown<date){
-                document.getElementById("over").innerHTML = "Please enter the present/future date";
+        var currentDate = new Date().getTime();
+      
+        if (countdown < currentDate) {
+          document.getElementById("over").innerHTML = "Please enter the present/future date";
+          udateElement.setAttribute("disabled", "disabled"); 
+          return; 
         }
-        else{
-        var distance = countdown - date;
+      
+        var distance = countdown - currentDate;
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -16,10 +19,11 @@ var start = function Timer(){
         document.getElementById("hourp").innerHTML = hours;
         document.getElementById("minp").innerHTML = minutes;
         document.getElementById("secp").innerHTML = seconds;
-        }
-
-}
-
-function stopClock() {
+      
+        udateElement.removeAttribute("disabled"); 
+      }
+      
+      function stopClock() {
         location.reload();
- }
+      }
+      
